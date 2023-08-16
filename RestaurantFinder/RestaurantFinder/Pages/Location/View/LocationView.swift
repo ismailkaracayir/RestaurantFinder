5e79f0da0780b02e65368c178aa7a9998763dd11
İsmail Karaçayır <83426543+ismailkaracayir@users.noreply.github.com>
2023-08-16 18:56:05 +0300

    1

---
+++ RestaurantFinder/RestaurantFinder/Pages/Location/View/LocationView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationView.swift
+//  RestaurantFinder
+//
+//  Created by ismail karaçayır on 16.08.2023.
+//
+
+import UIKit
+ 
+ @IBDesignable class LocationView: CoreView {
+     @IBOutlet weak var btnPermision : UIButton!
+     @IBOutlet weak var btnNotAllow : UIButton!
+
+     
+     @IBAction func btnPermision(_ serder: UIButton){}
+     @IBAction func btnNotAllow(_ serder: UIButton){}
+
+}
